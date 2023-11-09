9c80872f73b3de1afc39561eaa57e09cf7b11ba9
Your Name <you@example.com>
2023-11-09 18:33:39 +0700

    updata login

--- app/src/main/java/com/example/du_an_1/controller/view/LoginActivity.java
+++ app/src/main/java/com/example/du_an_1/controller/view/LoginActivity.java
@@ -9,8 +9,13 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.du_an_1.MainActivity;
+import com.example.du_an_1.database.EmployeeDao;
 import com.example.du_an_1.databinding.ActivityLoginBinding;
+import com.example.du_an_1.desgin_pattern.single_pantter.AccountSingle;
 import com.example.du_an_1.funtions.Validations;
+import com.example.du_an_1.model.Employee;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding loginBinding;
@@ -24,14 +29,47 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        Validations.isEmpty(edtUserName);
-        Validations.isEmpty(edtPassWord);
+        Validations.isPhoneNumber(loginBinding.userName);
+        Validations.isEmpty(loginBinding.password);
         loginBinding.btnLoginLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!Validations.isEmptyPress(edtUserName) && !Validations.isEmptyPress(edtPassWord)) {
-
+                int count = 0;
+                if(!Validations.isEmptyPress(loginBinding.userName)){
+                    if(!Validations.isPhoneNumberPress(loginBinding.userName)){
+                        count++;
+                    }
+                }else {
+                    count ++;
                 }
+                if (Validations.isEmptyPress(loginBinding.password)){
+                    count ++;
+                }
+
+                if (count != 0){
+                    return;
+                }
+
+                // sử lý logic login ở đây
+                EmployeeDao.getEmployees(new EmployeeDao.GetData() {
+                    // đọc dữ liệu từ fire base ra
+                    @Override
+                    public void getData(ArrayList<Employee> employees) {
+                        String numberPhone = loginBinding.userName.getText().toString();
+                        String pass = loginBinding.password.getText().toString();
+                        employees.forEach(o -> {
+                            if (numberPhone.equals(o.getNumberPhone())
+                                    && pass.equals(o.getPassword())){
+                                AccountSingle.getInstance().setAccount(o);
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                finish();
+                            }else {
+                                // sau cho một cái dialog ở đây nha
+                                Toast.makeText(LoginActivity.this,"Sai mât khẩu",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
             }
         });
     }
